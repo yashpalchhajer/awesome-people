733c9255b76c943998287999cee70df502f5032c
Nitish Kumar <mintu.nitish@gmail.com>
2018-06-12 17:48:10 +0530

    Tenant COntroller
    Signed-off-by: Nitish Kumar <nitish.kumar@sugaldamani.com>

---
+++ app/Http/Controllers/HomeDomain/TenantController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nitishkumar
+ * Date: 12/6/18
+ * Time: 5:37 PM
+ */
+
+namespace App\Http\Controllers\HomeDomain;
+
+use App\Http\Controllers\Controller;
+
+class TenantController extends Controller
+{
+    public function create()
+    {}
+
+    public function delete()
+    {}
+}
